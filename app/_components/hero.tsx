03b7619c330e2de33ac328a141d6e5af82a230cc
Noah <noahjoh11@gmail.com>
2024-01-31 15:22:37 +0530

    Created Countdown Timer UI for mobile screen and updated event date

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -12,7 +12,8 @@ interface Time {
 }
 
 const Hero = () => {
-  const targetDate = new Date("2024-04-01");
+  const targetDate = new Date("2024-02-28");
+  const eventDays = ["28", "29", "01"];
 
   const calculateTimeLeft = (targetDate: Date) => {
     const difference = targetDate?.getTime() - new Date().getTime();
@@ -37,13 +38,18 @@ const Hero = () => {
     seconds: 0,
   });
 
+  const [i, setI] = useState<number>(0);
+  const [eventDay, setEventDay] = useState<string>(eventDays[0]);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setTimeLeft(calculateTimeLeft(targetDate));
+      setEventDay(eventDays[i]);
+      setI((prevI) => (prevI + 1) % eventDays.length);
     }, 1000);
 
     return () => clearTimeout(timer);
-  });
+  }, [i]);
 
   let days = String(timeLeft?.days);
   days?.length < 2 ? (days = days.padStart(2, "0")) : days;
@@ -73,7 +79,7 @@ const Hero = () => {
       </div>
 
       {/* Countdown */}
-      <section className="relative w-[632px] text-secondary font-secondary hidden mt-12 md:flex md:items-center md:justify-center md:scale-75 lg:scale-100">
+      <section className="relative w-[632px] text-secondary font-secondary hidden mt-12 sm:flex items-center justify-center sm:scale-75 lg:scale-100">
         {/* Timer */}
         <div className="absolute flex gap-4 left-10 w-fit">
           <div className="flex flex-col text-center w-fit -space-y-1">
@@ -96,16 +102,63 @@ const Hero = () => {
             <p className="font-medium text-md text-primary">SECONDS</p>
           </div>
         </div>
+
         {/* Date */}
-        <div className="absolute flex flex-col right-4 text-center w-fit">
-          <p className="font-bold text-4xl text-primary">01-03</p>
-          <div className="flex w-fit">
-            <p className="font-extrabold text-md">April</p>
-            <p className="font-medium text-md">, 2024</p>
-          </div>
+        <div className="absolute flex flex-col right-7 text-center w-fit">
+          <p className="font-bold text-6xl text-primary">{eventDay}</p>
+          <p className="flex w-fit text-md">
+            <span className="font-extrabold text-md">
+              {eventDay === "01" ? "MAR" : "FEB"}
+            </span>
+            <span className="font-medium text-md">, 2024</span>
+          </p>
         </div>
         <div className="absolute">
           <CountdownWrapper />
+        </div>
+      </section>
+
+      {/* Countdown */}
+      <section className="relative w-[275px] h-[496px] text-secondary font-secondary -mt-28 flex sm:hidden items-center justify-center scale-75">
+        {/* Timer */}
+        <div className="absolute flex flex-col gap-4 left-11 top-14 w-fit">
+          <div className="flex gap-4">
+            <div className="flex flex-col text-center w-fit -space-y-1">
+              <p className="font-bold text-6xl">{days}</p>
+              <p className="font-medium text-md text-primary">DAYS</p>
+            </div>
+            <p className="font-bold text-6xl">:</p>
+            <div className="flex flex-col text-center w-fit -space-y-1">
+              <p className="font-bold text-6xl">{hours}</p>
+              <p className="font-medium text-md text-primary">HOURS</p>
+            </div>
+          </div>
+
+          <div className="flex gap-4">
+            <div className="flex flex-col text-center w-fit -space-y-1">
+              <p className="font-bold text-6xl">{minutes}</p>
+              <p className="font-medium text-md text-primary">MINUTES</p>
+            </div>
+            <p className="font-bold text-6xl">:</p>
+            <div className="flex flex-col text-center w-fit -space-y-1">
+              <p className="font-bold text-6xl">{seconds}</p>
+              <p className="font-medium text-md text-primary">SECONDS</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Date */}
+        <div className="absolute flex flex-col justify-center text-center w-fit bottom-10">
+          <p className="font-bold text-8xl text-primary">{eventDay}</p>
+          <p className="flex w-fit text-center mx-auto text-2xl">
+            <span className="font-extrabold">
+              {eventDay === "01" ? "MAR" : "FEB"}
+            </span>
+            <span className="font-medium">, 2024</span>
+          </p>
+        </div>
+        <div className="absolute">
+          <CountdownWrapper size="small" />
         </div>
       </section>
     </div>
--- components/svg/countdown-wrapper.tsx
+++ components/svg/countdown-wrapper.tsx
@@ -1,18 +1,39 @@
-const CountdownWrapper = () => (
-  <svg
-    width="646"
-    height="135"
-    viewBox="0 0 646 135"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      opacity="0.5"
-      d="M1.401 25.1668C1.18047 18.4757 6.47769 12.8981 13.1713 12.7736L633.021 1.24147C640.018 1.11129 645.642 6.97205 645.223 13.9579L638.677 123.084C638.297 129.421 633.047 134.366 626.698 134.366H16.611C10.1375 134.366 4.83077 129.231 4.61753 122.761L1.401 25.1668Z"
-      stroke="#DFFF1B"
-    />
-    <path opacity="0.5" d="M509 4L488.162 133.347" stroke="#DFFF1B" />
-  </svg>
-);
+interface CountdownWrapperProps {
+  size?: "small" | "large";
+}
+
+const CountdownWrapper = ({ size = "large" }: CountdownWrapperProps) => {
+  return size === "large" ? (
+    <svg
+      width="646"
+      height="135"
+      viewBox="0 0 646 135"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        opacity="0.5"
+        d="M1.401 25.1668C1.18047 18.4757 6.47769 12.8981 13.1713 12.7736L633.021 1.24147C640.018 1.11129 645.642 6.97205 645.223 13.9579L638.677 123.084C638.297 129.421 633.047 134.366 626.698 134.366H16.611C10.1375 134.366 4.83077 129.231 4.61753 122.761L1.401 25.1668Z"
+        stroke="#DFFF1B"
+      />
+      <path opacity="0.5" d="M509 4L488.162 133.347" stroke="#DFFF1B" />
+    </svg>
+  ) : (
+    <svg
+      width="277"
+      height="497"
+      viewBox="0 0 277 497"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        opacity="0.5"
+        d="M2.62301 32.8746C2.66904 19.9879 12.884 9.43767 25.7626 8.97573L250.967 0.897893C264.616 0.408333 275.921 11.3911 275.827 25.0482L272.738 472.29C272.645 485.819 261.394 496.597 247.873 496.109L24.217 488.035C11.2743 487.568 1.03673 476.916 1.08299 463.965L2.62301 32.8746Z"
+        stroke="#DFFF1B"
+      />
+      <path d="M2 302L274 283" stroke="#DFFF1B" stroke-linecap="round" />
+    </svg>
+  );
+};
 
 export default CountdownWrapper;
