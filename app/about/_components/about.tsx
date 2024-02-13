18564c82194d48cb190415d9a932e6a198eb6a34
navaneethku <navaneethku1729@gmail.com>
2024-02-13 12:32:08 +0530

    add images, edit gallery and about files

--- app/about/_components/about.tsx
+++ app/about/_components/about.tsx
@@ -13,15 +13,38 @@ const About = () => {
           <CustomText highlightedDescription>Techletics Tech Fest</CustomText>
           &nbsp;at Christ College of Engineering.&nbsp;
           <CustomText highlightedDescription>
-            Experience the latest technology and innovations in various fields of engineering.&nbsp;
+            Experience the latest technology and innovations in various fields
+            of engineering.&nbsp;
           </CustomText>
-          Engage with industry experts, network with peers, and gain valuable insights into the future of technology. Participate in exciting competitions, workshops, and interactive sessions. Expand your knowledge, enhance your skills, and be a part of the vibrant tech community at Christ College of Engineering.&nbsp;
+          Engage with industry experts, network with peers, and gain valuable
+          insights into the future of technology. Participate in exciting
+          competitions, workshops, and interactive sessions. Expand your
+          knowledge, enhance your skills, and be a part of the vibrant tech
+          community at Christ College of Engineering.&nbsp;
           <CustomText highlightedDescription>
-            Don&apos;t miss this opportunity to explore and be inspired by the ever-evolving world of engineering.&nbsp;
+            Don&apos;t miss this opportunity to explore and be inspired by the
+            ever-evolving world of engineering.&nbsp;
           </CustomText>
           Join us for the Techletics Tech Fest at Christ College of Engineering.
         </CustomText>
       </div>
+      <CustomText description>
+        <CustomText highlightedDescription>Techletics '22</CustomText>
+        &nbsp;is a techno-cultural techfest organized by Christ College of
+        Engineering, Irinjalakuda in 2022.&nbsp;
+        <CustomText highlightedDescription>
+          The techfest aims to provide a platform for technical students and
+          develop students and people from all walks of life.&nbsp;
+        </CustomText>
+        It is an opportunity to showcase their skills, learn from industry
+        experts, and network with peers. Participants can expect a wide range of
+        activities including competitions, workshops, and interactive sessions.
+        Techletics '22 is not just about technology, but also about fostering
+        creativity, innovation, and collaboration.&nbsp;
+        <CustomText highlightedDescription>
+          Join us for an unforgettable experience at Techletics '22!
+        </CustomText>
+      </CustomText>
     </div>
   );
 };
--- app/about/_components/gallery.tsx
+++ app/about/_components/gallery.tsx
@@ -4,44 +4,76 @@ import Image from "next/image";
 
 export default function PhotoGallery() {
   const images = [
-    "/images/legacy_image_1.jpg",
-    "/images/legacy_image_1.jpg",
-    "/images/legacy_image_1.jpg",
-    "/images/legacy_image_2.jpg",
-    "/images/legacy_image_2.jpg",
-    "/images/legacy_image_2.jpg",
+    "/images/legacy/1.jpg",
+    "/images/legacy/2.jpg",
+    "/images/legacy/3.jpg",
+    "/images/legacy/4.jpg",
+    "/images/legacy/5.jpg",
+    "/images/legacy/6.jpg",
+    "/images/legacy/7.jpg",
+    "/images/legacy/8.jpg",
+    "/images/legacy/9.jpg",
+    "/images/legacy/10.jpg",
+    "/images/legacy/11.jpg",
+    "/images/legacy/12.jpg",
+    "/images/legacy/13.jpg",
+    "/images/legacy/14.jpg",
+    "/images/legacy/15.jpg",
+    "/images/legacy/16.jpg",
+    "/images/legacy/17.jpg",
+    "/images/legacy/18.jpg",
+    "/images/legacy/19.jpg",
+    "/images/legacy/20.jpg",
+    "/images/legacy/21.jpg",
+    "/images/legacy/22.jpg",
+    "/images/legacy/23.jpg",
+    "/images/legacy/24.jpg",
+    "/images/legacy/25.jpg",
+    "/images/legacy/26.jpg",
+    "/images/legacy/27.jpg",
+    "/images/legacy/28.jpg",
   ];
-  const [currentBatchStartIndex, setCurrentBatchStartIndex] = useState(0);
+  const [currentBatch, setCurrentBatch] = useState<string[]>([]);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentBatchStartIndex((prevIndex) => (prevIndex + 3) % images.length);
-    }, 3000);
+      setCurrentBatch((prevBatch) => {
+        const randomIndexes = getRandomIndexes(images.length, 3);
+        const randomImages = randomIndexes.map((index) => images[index]);
+        return randomImages;
+      });
+    }, 5000);
 
     return () => {
       clearInterval(timer);
     };
   }, []);
 
+  const getRandomIndexes = (max: number, count: number): number[] => {
+    const indexes = Array.from({ length: max }, (_, i) => i);
+    const shuffledIndexes = indexes.sort(() => Math.random() - 0.5);
+    return shuffledIndexes.slice(0, count);
+  };
+
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
-      {images
-        .slice(currentBatchStartIndex, currentBatchStartIndex + 3)
-        .map((image, index) => (
-          <div
-            key={index}
-            className="col-span-1 transition duration-500 ease-in-out"
-          >
-            <div className="saturate-0 transition duration-500 ease-in-out hover:saturate-100">
-              <Image
-                src={image}
-                alt={`Image ${index + 1}`}
-                width={500}
-                height={300}
-              />
-            </div>
+      {currentBatch.map((image, index) => (
+        <div
+          key={index}
+          className="col-span-1 transition duration-500 ease-in-out"
+        >
+          <div className="saturate-0 transition duration-500 ease-in-out hover:saturate-100">
+            <Image
+              loading="lazy"
+              src={image}
+              alt={`Image ${index + 1}`}
+              width={500}
+              height={300}
+              className="h-96 w-full transform object-cover transition-all duration-500 hover:scale-105"
+            />
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   );
 }
