864b3c636f7ea045f92ed1a3edcb0769635eb848
sai-prasad-1 <sai.prasad.1603@gmail.com>
2024-02-15 03:01:44 +0530

    Added Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,23 @@ import { CustomCursorProvider } from "@/context/custom-cursor-context";
 import CustomCursor from "@/components/custom/cursor";
 
 export const metadata: Metadata = {
-  title: "Techletics '24",
+  title: {
+    template: "%s | Techletics '24",
+    default: "Techletics '24 | Ignite Inspire Illuminate",
+  
+
+  },
+  keywords: "Techletics, Tech Fest, Christ College of Engineering, Irinjalakuda",
   description:
     "Tech Fest organized by Christ College of Engineering, Irinjalakuda",
+  creator:"Website Team CCE",
+  openGraph:{
+    title :"Techletics '24 | Ignite Inspire Illuminate",
+    description: "Tech Fest organized by Christ College of Engineering, Irinjalakuda",
+    type : "website",
+    url : "https://techletics.cce.edu.in/",
+    locale :"en_US",
+  }
 };
 
 export default function RootLayout({
@@ -21,6 +35,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logos/techletics-metal.svg" />
+        <meta property="og:image" content={"/images/techletics-metal.svg"} />
+        <meta property="og:instagram:app_id" content="techleticscce" />
+
+        
       </head>
 
       <body
