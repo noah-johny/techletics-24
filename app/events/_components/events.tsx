7691984ffd0909808dd9dba4558bf9016e5a6673
Noah <noahjoh11@gmail.com>
2024-02-21 09:23:05 +0530

    Added event registration links for redirection

--- app/events/_components/events.tsx
+++ app/events/_components/events.tsx
@@ -4,187 +4,12 @@ import { useState, useCallback } from "react";
 import Image from "next/image";
 import SectionLayout from "@/layouts/section-layout";
 import { CustomText } from "@/components/custom";
-
-type Event = {
-  name: string;
-  url: string;
-  branch?: string;
-  community?: string;
-  category: string;
-  type?: string;
-};
+import { eventList } from "./event-list";
+import Link from "next/link";
 
 const branches = ["ALL", "CSE", "ME", "CE", "EC", "EEE", "BSH"];
 const types = ["ALL EVENTS", "COMPETITION", "WORKSHOP", "TECH-TALK", "EXPO"];
 const categories = ["TECHNICAL", "CULTURAL"];
-
-const eventList: Event[] = [
-  {
-    name: "Women in Buisness",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/women-in-business_.png",
-    community: "IEDC",
-    category: "TECHNICAL",
-    type: "EXPO",
-  },
-  {
-    name: "Idea Pitching",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-18_at_22.29.37_c9735fa3.jpg",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "EV Drive",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-18_at_22.29.13_5f8481ef.jpg",
-    branch: "EEE",
-    category: "TECHNICAL",
-    type: "WORKSHOP",
-  },
-  {
-    name: "Hacknite",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-18_at_22.29.08_791165df.jpg",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Technical Quiz",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-18_at_14.33.25_d5a52fd6.jpg",
-    branch: "CE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "E-Football Tournament",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-17_at_23.29.49_1e9e2961.jpg",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "BGMI Tournament",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-17_at_23.29.38_60d3cb58.jpg",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Capture The Flag: Cyber Challenge",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-17_at_23.29.25_caaf84b1.jpg",
-    community: "IEEE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Tech Talks: Life of an Entrepreneur",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-17_at_23.29.05_52c66f9e.jpg",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "TECH-TALK",
-  },
-  {
-    name: "Tech Talks: Cyber Security",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-17_at_23.29.04_0ffb2014.jpg",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "TECH-TALK",
-  },
-  {
-    name: "Non Destructive Testing",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-15_at_22.37.09_e6b94974-min.jpg",
-    branch: "CE",
-    category: "TECHNICAL",
-    type: "WORKSHOP",
-  },
-  {
-    name: "Robo Soccer",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-15_at_22.36.07_69120677-min.jpg",
-    branch: "EC",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Da Vinci's Vault",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-15_at_22.32.31_120de47f-min.jpg",
-    branch: "ME",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: " EV Hack",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-15_at_22.32.30_63b4b103-min.jpg",
-    branch: "EEE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Project Expo",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-13_at_22.00.36_d0ea6677-min.jpg",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "EXPO",
-  },
-  {
-    name: "Quake Proof Creation Challenge",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-13_at_22.00.19_9e5926ea-min.jpg",
-    branch: "CE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: " Wrenching Wheels",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-13_at_19.31.05_d777ea88-min.jpg",
-    branch: "ME",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Malhar: Musical Band Competition",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-13_at_19.30.52_89e38456-min.jpg",
-    category: "CULTURAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "iRobotics Workshop",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-20_at_11.49.45_db67373d.jpg",
-    branch: "ECE",
-    category: "TECHNICAL",
-    type: "WORKSHOP",
-  },
-  {
-    name: "Build your Dreams",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-20_at_11.49.27_012367d9.jpg",
-    branch: "CE",
-    category: "EXPO",
-  },
-  {
-    name: "Clues and Crayns",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/WhatsApp_Image_2024-02-20_at_11.49.22_f2a885d2.jpg",
-    branch: "ME",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Designathon",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/designathon.png",
-    branch: "CSE",
-    category: "TECHNICAL",
-    type: "COMPETITION",
-  },
-  {
-    name: "Drishti",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/dristi.jpg",
-    branch: "BSH",
-    category: "TECHNICAL",
-    type: "EXPO",
-  },
-  {
-    name: "BeatSync Battle",
-    url: "https://dnbca6q7do6n.cloudfront.net/media/techletics24/dance-group.png",
-    category: "CULTURAL",
-    type: "COMPETITION",
-  },
-];
 
 const Events = () => {
   const [activeBranch, setActiveBranch] = useState("ALL");
@@ -267,18 +92,20 @@ const Events = () => {
                 (activeType === "ALL EVENTS" || event.type === activeType),
             )
             .map((event) => (
-              <div
-                className="h-72 w-64 p-5 transition-all duration-200 ease-in hover:scale-125"
-                key={event.name}
-              >
-                <Image
-                  className="h-full w-full object-contain grayscale hover:grayscale-0"
-                  src={event.url}
-                  alt={event.name}
-                  width={300}
-                  height={300}
-                />
-              </div>
+              <Link href={event.url}>
+                <div
+                  className="h-72 w-64 p-5 transition-all duration-200 ease-in hover:scale-125"
+                  key={event.name}
+                >
+                  <Image
+                    className="h-full w-full object-contain grayscale hover:grayscale-0"
+                    src={event.src}
+                    alt={event.name}
+                    width={300}
+                    height={300}
+                  />
+                </div>
+              </Link>
             ))}
         </div>
       </div>
