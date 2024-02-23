ea2b19eda93b202cd9a559eb45b3d2397e48c52e
Pranav Madhu <pranavmadhu73@gmail.com>
2024-02-23 23:37:34 +0530

    alumini form boilerplate setup

--- app/(notprotected)/alumini/page.tsx
+++ app/(notprotected)/alumini/page.tsx
@@ -1,5 +1,128 @@
+import SectionLayout from "@/layouts/section-layout";
+import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import Title from "@/components/title";
+import { CustomText } from "@/components/custom";
+import React from "react";
 export default function AluminiPage() {
     return (
-        <div className="pt-32">This is alumini page</div>
+        <>
+            <div className="mt-[72px] sm:mt-[88px] xl:mt-24"></div>
+            <Title title="alumini" />
+            <SectionLayout full>
+                <form className="bg-secondary py-2 sm:py-4 lg:py-9 px-4 sm:px-8 lg:px-16">
+                    <div className="space-y-12">
+                        <div className="border-b border-gray-900/10 pb-12">
+                            <CustomText highlightedTitle><span className="text-black">registration form</span></CustomText>
+                            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-4">
+                                    <label htmlFor="fullname" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Full Name
+                                    </label>
+                                    <div className="mt-2">
+                                        <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                            <input
+                                                type="text"
+                                                name="fullname"
+                                                id="fullname"
+                                                autoComplete="fullname"
+                                                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                placeholder="John Doe"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="sm:col-span-4">
+                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Email address
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            id="email"
+                                            name="email"
+                                            type="email"
+                                            autoComplete="email"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="sm:col-span-4">
+                                    <label htmlFor="phone" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Phone Number
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            id="phone"
+                                            name="phone"
+                                            type="tel"
+                                            autoComplete="phone"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="sm:col-span-3">
+                                    <label htmlFor="passout" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Passout Year
+                                    </label>
+                                    <div className="mt-2">
+                                        <select
+                                            id="passout"
+                                            name="passout"
+                                            autoComplete="country-name"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                                        >
+                                            <option value="">2019</option>
+                                            <option value="">2020</option>
+                                            <option value="">2021</option>
+                                            <option value="">2022</option>
+                                            <option value="">2023</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-span-full">
+                                    <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Photo
+                                    </label>
+                                    <div className="mt-2 flex items-center gap-x-3">
+                                        <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+                                        <button
+                                            type="button"
+                                            className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                        >
+                                            Upload
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="col-span-full">
+                                    <label htmlFor="suggestions" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Suggestions
+                                    </label>
+                                    <div className="mt-2">
+                                        <textarea
+                                            id="suggestions"
+                                            name="suggestions"
+                                            rows={3}
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            defaultValue={''}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                            Reset
+                        </button>
+                        <button
+                            type="submit"
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </SectionLayout >
+        </>
     )
 }
